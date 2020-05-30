8e51917007e65271ea94c740e1af91df681577d5
concourse008 <61238161+concourse008@users.noreply.github.com>
2020-05-30 15:05:22 +0900

    Add files via upload

--- move.js
+++ move.js
@@ -14,10 +14,11 @@ let ctx = canvas[flip].getContext('2d');
 const ctx0 = canvas[2].getContext('2d');
 const srcs = [
   ['back.png',0,0],
-  ['mito.png',40,240],
-  ['obs.png',400,280],
+  ['mito2.png',40,240],
+  ['obs2.png',400,280],
   ['gameover.png',200,100],
-  ['op.png',0,0]
+  ['op.png',0,0],
+  ['mitojump.png',0,0]
 ];
 
 let images = [];
@@ -54,7 +55,6 @@ canvas[2].addEventListener('click',e =>{
   //クリック判定処理
   jumpAble = false;
   console.log('c');
-  hiscore = hiscore +10;
   //停止中、クリックでスタート
   if (stop == true){
     stop = false;
@@ -63,7 +63,7 @@ canvas[2].addEventListener('click',e =>{
 })
 
 let x = 40;
-let y = 240;
+let y = 20;
 let n = 0;
 let obs = [400,400,400];
 let lastgo = 0;
@@ -86,7 +86,11 @@ function move(){//毎1/10秒の更新
   }
   ctx.clearRect(0, 0, 400, 400);
   ctx.drawImage(images[0],0,0);
-  ctx.drawImage(images[1],x,y);
+  if (jumpAble == false){
+    ctx.drawImage(images[5],x,y);
+  }else{
+    ctx.drawImage(images[1],x,y);
+  }
   score();
   hiscore++;
   obstacle();
@@ -97,15 +101,17 @@ let hiscore = 0;
 function score() {
   ctx.font = "18px serif";
   let i = Math.floor(hiscore / 10);
-  ctx.fillText("score:" + i , 300, 30);
+  ctx.fillText("score:" + i , 250, 30);
 }
 
 //障害物の表示、動き
+let speed = [6,7,8,9];
+let randam = 0;
 function obstacle(){
   for (let i in obs){
     if (obs[i] == 400){
-      if (Math.floor(Math.random()*30) === 0 && lastgo >= 40){
-        ctx.drawImage(images[2],obs[i],280);
+      if (Math.floor(Math.random()*30) === 0 && lastgo >= 45){
+        ctx.drawImage(images[2],obs[i],275);
         obs[i] = obs[i] -10;
         console.log('go');
         lastgo = 0;
@@ -114,16 +120,18 @@ function obstacle(){
     }else if(obs[i] <= -15){
       obs[i] = 400;
     }else{
-      ctx.drawImage(images[2],obs[i],280);
-      obs[i] = obs[i] - 7;//障害物のスピード
-      console.log('move');
+      ctx.drawImage(images[2],obs[i],275);
+      if (hiscore % 300 == 0){
+      randam = Math.floor(Math.random()*4);
+      }
+      obs[i] = obs[i] - speed[randam];//障害物のスピード
     }
   }
 }
 //当たり判定
 function hitcheck(){
   for (let i in obs){
-    if (obs[i] <= 82 && obs[i] >= 20 && y >= 225){
+    if (obs[i] <= 82 && obs[i] >= 35 && y >= 205){
       gameover();
       console.log('hit');
     }
@@ -133,12 +141,19 @@ function gameover(){
   ctx0.drawImage(images[3],100,100);
   clearInterval(moveing);
   stop = true;
+  //ハイスコア記録
+  if (totalscore <= hiscore){
+  localStorage.score = hiscore;
+  }
+  tweet();
 }
 
 let moveing = 0;
 function restart(){
   ctx0.clearRect(0, 0, 400, 400);
-  y = 240;
+  ctx0.font = "18px serif";
+  ctx0.fillText("high score:" + Math.floor(totalscore / 10) , 50, 30);
+  y = 220;
   n = 0;
   obs = [400,400,400];
   lastgo = 0;
@@ -148,6 +163,56 @@ function restart(){
     moveing = setInterval(move,1000/30);
   }
   moveng();
+}
+
+//ハイスコア
+let totalscore = 0;
+function getup(){
+  localStorage.setItem('score',0);
+  totalscore = Number(localStorage.getItem('score'));
+}
+function setup(){
+  totalscore = Number(localStorage.getItem('score'));
+}
+if(!localStorage.getItem('score')) {
+  //ストレージなし
+  getup();
+} else {
+  //ストレージあり
+  setup();
+}
+
+//ツイートボタン
+const tweetDivided = document.getElementById('tweet-area');
+function tweet(){
+  while( tweetDivided.firstChild ){
+    tweetDivided.removeChild( tweetDivided.firstChild );
+  }
+  let way = Math.floor(hiscore / 10);
+  let coment = 0;
+  if (hiscore >= 3000){
+    coment = '「ミトちゃんかぜになってたわ」';
+  }else if(hiscore >= 2000){
+    coment = '「ミトちゃんはすごいなぁ」';
+  }else if(hiscore >= 1000){
+    coment = '「ええきもちや」';
+  }else if(hiscore >= 500){
+    coment = '「なんやこのしかくいうんち」';
+  }else{
+    coment = '「ミトちゃんあっちになんかある！」';
+  }
+  const anchor = document.createElement('a');
+  const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' + encodeURIComponent('カエデログ') + '&ref_src=twsrc%5Etfw';
+  anchor.setAttribute('herf',hrefValue);
+  anchor.className = 'twitter-hashtag-button';
+  anchor.setAttribute('data-text','ミトとカエデは'+way+'メートルを走り抜けました！　'+coment+'　#ミトダッシュ');
+  anchor.setAttribute('data-size',"large");
+  anchor.setAttribute('data-url',"https://concourse008.github.io/mitodash/index.html");
+  anchor.innerText = 'Tweet #カエデログ';
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+  tweetDivided.appendChild(script);
+  tweetDivided.appendChild(anchor);
 }
 
 /*ジャンプし続ける
@@ -182,4 +247,5 @@ function jump(){
   console.log(y);
 }
 setInterval(jump,100);
+<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 */
